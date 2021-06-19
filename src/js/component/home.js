0983ad081981d5e09b4487a6d22f64dc016c8167
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-06-19 14:23:34 +0000

    Version 2.0: Agreada la funcion de eliminar

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,34 +1,38 @@
 import React, { useState, setState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-let listaTareasa = ["Tarea 1", "Tarea 2", "Tarea 3"];
+let listaTareas = ["Tarea 1", "Tarea 2", "Tarea 3"];
 
 let state = { value: "" };
 
 export function Home() {
-	const [listaTareas, setListaTareas] = useState([
-		"Tarea 1",
-		"Tarea 2",
-		"Tarea 3"
-	]);
+	const [numTareas, setNumTareas] = useState(0);
 
 	function agregar() {
-		listaTareasa.push("Luis");
-		setListaTareas(listaTareasa);
+		let aux = numTareas;
+		aux++;
+		listaTareas.push(this.state.value);
+		this.setState("");
+		setNumTareas(aux);
+		return;
+	}
+
+	function eliminar() {
+		let aux = numTareas;
+		aux--;
+		listaTareas.pop();
+		setNumTareas(aux);
 		return;
 	}
 
 	return (
 		<div className="container d-flex flex-column align-items-center">
 			<h1 className="style-1 my-4">To-Do List</h1>
-			<ul className="list-group libreta">
+			<ul className="list-group shadow">
 				<input
 					className="list-group-item caja-todo"
-					onChange={descripcion =>
-						this.setState({ value: descripcion.target.value })
+					placeholder="Escribir una nueva tarea..."
+					onChange={e =>
+						this.setState({ value: e.target.value })
 					}></input>
 				{listaTareas.length === 0 ? (
 					<li className="list-group-item caja-todo">
@@ -43,7 +47,8 @@ export function Home() {
 								<p className="my-2">{name}</p>
 								<button
 									type="button"
-									className="btn btn-link hide">
+									className="btn btn-link hide"
+									onClick={eliminar}>
 									<i className="fas fa-times"></i>
 								</button>
 							</li>
@@ -64,7 +69,7 @@ export function Home() {
 			</ul>
 			<button
 				type="button"
-				className="btn btn-info mt-3"
+				className="btn btn-info my-3 shadow"
 				onClick={agregar}>
 				Agregar Tarea
 			</button>
